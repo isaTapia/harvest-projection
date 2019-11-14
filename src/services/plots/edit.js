076558b4f7a3b7ff3712ada16c306e7030095c9f
Carlos Oliva <olivam@protonmail.com>
2019-11-14 15:56:42 -0800

    Refactorice plots/edit

--- src/services/plots/edit.js
+++ src/services/plots/edit.js
@@ -4,15 +4,21 @@ const ServicesFactory = require('../services-factory')
 
 
 
-// [TODO] aquí hay un bug donde el usuario puede editar parcelas que no son suyas
-module.exports = ServicesFactory.createItemEditionService(
-  Plot, 
-  function(request) {
-    return {
-      name: request.body.name,
-      latitude: request.body.latitude,
-      longitude: request.body.longitude
-    }
-  },
-  '_id name latitude longitude'
-)
+module.exports = ServicesFactory.createCustomService(async (request, response) => {
+  const id = request.params.id
+  const userId = request.decodedToken._id
+  let plot = await Plot.findById(id)
+  if (plot.owner.toString() !== userId) {
+    throw new Error('Can\'t delete plots that do not belong to you')
+  }
+
+  const data = request.body
+  const options = { 
+    new: true, 
+    select: '_id name latitude longitude',
+    omitUndefined: true
+  }
+
+  const item = await Plot.findByIdAndUpdate(id, data, options)
+  return item
+})
