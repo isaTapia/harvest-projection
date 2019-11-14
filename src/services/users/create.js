b7883e8855e03ab3e13ad6a453bcf3e54f2d94bd
Carlos Oliva <olivam@protonmail.com>
2019-11-14 15:42:53 -0800

    Refactorice users/create

--- src/services/users/create.js
+++ src/services/users/create.js
@@ -6,25 +6,20 @@ const bcrypt = require('bcrypt')
 
 
 // [TODO] editar ServicesFactory para que reciba la proyeccion antes que los callbacks
-module.exports = async function(request, response) {
-  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
-  try {
-    const hash = await bcrypt.hash(request.body.password, 1)
-    const data = {
-      name: request.body.name,
-      email: request.body.email,
-      password: hash
-    }
-    let createdUser = new User(data)
-    createdUser = await createdUser.save()
-    const result = {
-      _id: createdUser._id,
-      name: createdUser.name,
-      email: createdUser.email,
-      plotsList: createdUser.plotsList
-    }
-    response.json(result)
-  } catch (error) {
-    handleException(error)
+module.exports = ServicesFactory.createCustomService(async (request, response) => {
+  const hash = await bcrypt.hash(request.body.password, 1)
+  const data = {
+    name: request.body.name,
+    email: request.body.email,
+    password: hash
   }
-}
+  let createdUser = new User(data)
+  createdUser = await createdUser.save()
+  const result = {
+    _id: createdUser._id,
+    name: createdUser.name,
+    email: createdUser.email,
+    plotsList: createdUser.plotsList
+  }
+  return result
+})
