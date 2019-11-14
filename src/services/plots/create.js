3808afba128490d59afafc7ec2695b34ea0d28f6
Carlos Oliva <olivam@protonmail.com>
2019-11-14 14:45:57 -0800

    Refactorice servicio plots/create

--- src/services/plots/create.js
+++ src/services/plots/create.js
@@ -1,18 +1,29 @@
 const Plot = require('../../models/plot')
+const User = require('../../models/user')
 const ServicesFactory = require('../services-factory')
 
 
 
 
-module.exports = ServicesFactory.createItemCreationService(
-  Plot, 
-  function(request) { 
-    return {
-      userId: request.decodedToken._id,
-      name: request.body.name,
-      latitude: request.body.latitude,
-      longitude: request.body.longitude
-    }
-  },
-  '_id name latitude longitude'
-)
+// [TODO] de nuevo, hay que editar el webtoken para agregar el cultivo creado
+module.exports = async function(request, response) {
+  const handleError = ServicesFactory.createOnQueryRejectionCallback(response)
+  try {
+    const data = request.body
+    let plot = new Plot(data)
+
+    const userId = request.decodedToken._id
+    const user = await User
+      .findById(userId, '_id name email plotsList')
+      .populate('plotsList')
+    
+    plot.owner = user._id
+    plot = await plot.save()
+    user.plotsList.push(plot._id)
+    await user.save()
+    plot = await Plot.findById(plot._id, '_id name latitude longitude')
+    response.json(plot)
+  } catch (error) {
+    handleError(error)
+  }
+}
