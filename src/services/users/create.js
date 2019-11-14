9b00a0e379bc8d398bad7635e657389dc86bc687
Carlos Oliva <olivam@protonmail.com>
2019-11-14 13:51:56 -0800

    Refactoricé crear usuario

--- src/services/users/create.js
+++ src/services/users/create.js
@@ -6,38 +6,26 @@ const bcrypt = require('bcrypt')
 
 
 // [TODO] editar ServicesFactory para que reciba la proyeccion antes que los callbacks
-module.exports = function(request, response) {
-  const onPasswordHashed = (error, hash) => {
-    if (error) {
-      console.error(error)
-      throw new Error('Failed to hash the user password')
-    }
-
-    const onQueryRejection = ServicesFactory.createOnQueryRejectionCallback(response)
-    const onQueryFulfillment = document => {
-      if (document) {
-        const result = {}
-        const fieldsList = '_id name email'.split(' ')
-        for (field of fieldsList) {
-          result[field] = document.get(field)
-        }
-        result['plotsList'] = []
-        response.json(result)
-      } else {
-        response.json(document)
-      }
-    }
-
+module.exports = async function(request, response) {
+  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
+  try {
+    const hash = await bcrypt.hash(request.body.password, 1)
     const data = {
       name: request.body.name,
       email: request.body.email,
       password: hash
     }
-    const item = new User(data)
-    item.save()
-      .then(onQueryFulfillment)
-      .catch(onQueryRejection)
+    const createdUser = new User(data)
+    user = await createdUser.save()
+    const result = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      plotsList: user.plotsList
+    }
+    response.json(result)
+  } catch (error) {
+    console.error(error)
+    handleException(error)
   }
-
-  bcrypt.hash(request.body.password, 1, onPasswordHashed)
 }
