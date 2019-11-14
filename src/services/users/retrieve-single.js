e5cc2daa5c5c4de5dcbae7de422d824657710532
Carlos Oliva <olivam@protonmail.com>
2019-11-14 14:06:03 -0800

    Refactorice user/retrieve-single

--- src/services/users/retrieve-single.js
+++ src/services/users/retrieve-single.js
@@ -5,34 +5,15 @@ const ServicesFactory = require('../services-factory')
 
 
 
-module.exports = function(request, response) {
-  const onQueryRejection = ServicesFactory.createOnQueryRejectionCallback(response)
-  // const onQueryFulfillment = ServicesFactory.createOnQueryFulfillmentCallback(response)
-  const onUserFound = user => {
-    const onPlotsListRetrieved = plotsList => {
-      const result = {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        plotsList: plotsList
-      } 
-      response.json(result)
-    }
-    
-    const where = { userId: user._id }
-    Plot.findOne(where, '_id name latitude longitude')
-      .then(onPlotsListRetrieved)
-      .catch(onQueryRejection)
+module.exports = async function(request, response) {
+  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
+  try {
+    const id = request.decodedToken._id
+    const user = await User
+      .findById(id, '_id name email plotsList')
+      .populate('plotsList')
+    response.json(user)
+  } catch (error) {
+    handleException(error)
   }
-  
-  const id = request.decodedToken._id
-  const options = { select: '_id name email' }
-  model.findById(id, null, options)
-    .then(onUserFound)
-    .catch(onQueryRejection)
 }
-// ServicesFactory.createSingleItemRetrievalService(
-//   User, 
-//   (request) => request.decodedToken._id,
-//   '_id name email'
-// )
