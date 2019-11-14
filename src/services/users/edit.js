ddf81d67dbb0091b06d2497c0f85fd8bdbafa513
Carlos Oliva <olivam@protonmail.com>
2019-11-14 15:44:24 -0800

    Refactorice users/services

--- src/services/users/edit.js
+++ src/services/users/edit.js
@@ -7,25 +7,20 @@ const ServicesFactory = require('../services-factory')
 
 // [TODO] invalidar viejo token despues de cambiar los datos del usuario y 
 // retornar un nuevo token con los datos nuevos
-module.exports = async function(request, response) {
-  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
-  try {
-    const id = request.decodedToken._id
-    const data = request.body
+module.exports = ServicesFactory.createCustomService(async (request, response) => {
+  const id = request.decodedToken._id
+  const data = request.body
 
-    if (data.password) {
-      const hash = await bcrypt.hash(data.password, 1)
-      data.password = hash
-    }
-
-    const options = {
-      new: true,
-      select: '_id name email plotsList',
-      omitUndefined: true
-    }
-    const user = await User.findByIdAndUpdate(id, data, options)
-    response.json(user)
-  } catch (error) {
-    handleException(error)
+  if (data.password) {
+    const hash = await bcrypt.hash(data.password, 1)
+    data.password = hash
   }
-}
+
+  const options = {
+    new: true,
+    select: '_id name email plotsList',
+    omitUndefined: true
+  }
+  const user = await User.findByIdAndUpdate(id, data, options)
+  return user
+})
