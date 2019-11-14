b0a99da4fcc4ef680417b37e3bbc9ba64caf1465
Carlos Oliva <olivam@protonmail.com>
2019-11-14 15:46:44 -0800

    refactorice plots/create

--- src/services/plots/create.js
+++ src/services/plots/create.js
@@ -6,24 +6,19 @@ const ServicesFactory = require('../services-factory')
 
 
 // [TODO] de nuevo, hay que editar el webtoken para agregar el cultivo creado
-module.exports = async function(request, response) {
-  const handleError = ServicesFactory.createOnQueryRejectionCallback(response)
-  try {
-    const data = request.body
-    let plot = new Plot(data)
+module.exports = ServicesFactory.createCustomService(async (request, response) => {
+  const data = request.body
+  let plot = new Plot(data)
 
-    const userId = request.decodedToken._id
-    const user = await User
-      .findById(userId, '_id name email plotsList')
-      .populate('plotsList')
-    
-    plot.owner = user._id
-    plot = await plot.save()
-    user.plotsList.push(plot._id)
-    await user.save()
-    plot = await Plot.findById(plot._id, '_id name latitude longitude')
-    response.json(plot)
-  } catch (error) {
-    handleError(error)
-  }
-}
+  const userId = request.decodedToken._id
+  const user = await User
+    .findById(userId, '_id name email plotsList')
+    .populate('plotsList')
+  
+  plot.owner = user._id
+  plot = await plot.save()
+  user.plotsList.push(plot._id)
+  await user.save()
+  plot = await Plot.findById(plot._id, '_id name latitude longitude')
+  return plot
+})
