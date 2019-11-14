ae8c5dfe12f7d9068f9a6ae05a22bb71118beb33
Carlos Oliva <olivam@protonmail.com>
2019-11-14 15:45:21 -0800

    refactorice users/retrieve-single

--- src/services/users/retrieve-single.js
+++ src/services/users/retrieve-single.js
@@ -4,15 +4,10 @@ const ServicesFactory = require('../services-factory')
 
 
 
-module.exports = async function(request, response) {
-  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
-  try {
-    const id = request.decodedToken._id
-    const user = await User
-      .findById(id, '_id name email plotsList')
-      .populate('plotsList', '_id name latitude longitude')
-    response.json(user)
-  } catch (error) {
-    handleException(error)
-  }
-}
+module.exports = ServicesFactory.createCustomService(async (request, response) => {
+  const id = request.decodedToken._id
+  const user = await User
+    .findById(id, '_id name email plotsList')
+    .populate('plotsList', '_id name latitude longitude')
+  response.json(user)
+})
