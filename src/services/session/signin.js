94598fe8906d59447b11677f7a7b2f9e49b63175
Carlos Oliva <olivam@protonmail.com>
2019-11-14 14:00:29 -0800

    Refactoricé el servicio de inicio de sesión

--- src/services/session/signin.js
+++ src/services/session/signin.js
@@ -9,52 +9,39 @@ const webtoken = require('jsonwebtoken')
 
 // [TODO] ver si se pude implementar con async/await
 // [TODO] no todos los errores son capturados por express
-module.exports = function(request, response) {
-  const onQueryRejection = ServicesFactory.createOnQueryRejectionCallback(response)
-  const onUserEmailFound = user => {
+module.exports = async function(request, response) {
+  const handleException = ServicesFactory.createOnQueryRejectionCallback(response)
+  try {
+    const searchCriteria = { email: request.body.email }
+    const user = await User
+      .findOne(searchCriteria, '_id name email password plotsList')
+      .populate('plotsList')
+
     if (!user) {
-      console.info('User email not found during authentication')
+      console.info(`User email '${request.body.email}' not found during authentication`)
       throw new Error('User authentication failed')
     }
 
-    const onHashComparison = (error, areEqual) => {
-      if (error) {
-        console.error(error)
-        throw new Error('User authentication failed')
-      }
-
-      if (!areEqual) {
-        console.info('User password is not correct')
-        throw new Error('User authentication failed')
-      }
-
-      const config = {
-        expiresIn: '1h'
-      }
-      const token = webtoken.sign(user.toJSON(), process.env.JSON_WEB_TOKEN_SECRET_KEY, config)
-      
-      const onPlotsListRetrieved = plotsList => {
-        const result = {
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          token: token,
-          plotsList: plotsList
-        }
-        response.json(result)
-      }
-
-      const where = { userId: user._id }
-      Plot.find(where, '_id name latitude longitude')
-        .then(onPlotsListRetrieved)
-        .catch(onQueryRejection)
+    const areEqual = await bcrypt.compare(request.body.password, user.password)
+    if (!areEqual) {
+      console.info(`User '${request.body.email}' password is not correct`)
+      throw new Error('User authentication failed')
     }
 
-    bcrypt.compare(request.body.password, user.password, onHashComparison)
+    const config = {
+      expiresIn: '1h'
+    }
+    const token = webtoken.sign(user.toJSON(), process.env.JSON_WEB_TOKEN_SECRET_KEY, config)
+    const result = {
+      token: token,
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      plotsList: user.plotsList
+    }
+    response.json(result)
+  } catch (error) {
+    console.error(error)
+    handleException(error)
   }
-
-  const where = { email: request.body.email }
-  User.findOne(where, '_id name email password')
-    .then(onUserEmailFound)
-    .catch(onQueryRejection)
 }
